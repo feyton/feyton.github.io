ad030fa22185a59af120c316ee9a1a72ee0d6eb5
Fabrice Hafashimana <feytoninc@hotmail.com>
2021-11-26 11:57:50 +0200

    added a product section

--- main.js
+++ main.js
@@ -3,4 +3,28 @@ $(document).ready(function () {
     e.preventDefault();
     alert("Button clicked");
   });
+  $(".disabled").click(function (e) {
+    e.preventDefault();
+  });
+
+  function setCookie(cname, cvalue, exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
+    let expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+  }
+
+  function checkCartCookie() {
+    document.cookie = "cartItems = 0";
+    let x = document.cookie;
+  }
+  function addToCart() {}
+  function checkCartCookie() {
+    let cartItems = getCookie("cartItems");
+    if (username != "") {
+      setCookie("cartItems", 0, 5);
+    } else {
+      return undefined;
+    }
+  }
 });
